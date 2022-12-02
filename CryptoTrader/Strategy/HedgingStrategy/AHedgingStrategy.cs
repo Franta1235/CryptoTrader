ae91a6dbc87fcd8fcbf7de9f132fad321a50ccf1
Franta1235 <koznar.franta@gmail.com>
2022-12-02 15:27:04 +0100

    Hedging

--- CryptoTrader/Strategy/HedgingStrategy/AHedgingStrategy.cs
+++ CryptoTrader/Strategy/HedgingStrategy/AHedgingStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CryptoTrader.Helper;
+using CryptoTrader.Helper.Candle;
 using CryptoTrader.HistoryAnalyzer;
 
 namespace CryptoTrader.Strategy.HedgingStrategy
@@ -7,21 +8,35 @@ namespace CryptoTrader.Strategy.HedgingStrategy
     public abstract class AHedgingStrategy : IStrategy
     {
         protected IHistoryAnalyzer HistoryAnalyzer;
+        protected CandleInterval _candleInterval;
         protected Price CurrentPrice;
+        protected Price OpenPrice;
+        protected double LastOpenTimestamp;
 
 
-        public AHedgingStrategy(IHistoryAnalyzer historyAnalyzer) {
+        public AHedgingStrategy(IHistoryAnalyzer historyAnalyzer, CandleInterval candleInterval) {
             HistoryAnalyzer = historyAnalyzer;
+            _candleInterval = candleInterval;
         }
 
         public double Advance(Price price) {
             CurrentPrice = price;
             HistoryAnalyzer.Advance(price);
-            return Recalculate();
+            if (IsNewContract()) StartNewContract();
+            return MakeOrder();
         }
 
-        protected abstract double Recalculate();
+        private bool IsNewContract() {
+            var openTimestamp = CurrentPrice.Timestamp - CurrentPrice.Timestamp % (int) _candleInterval;
+            return Math.Abs(openTimestamp - LastOpenTimestamp) > 1;
+        }
+
+        protected abstract double MakeOrder();
+
+        protected abstract void StartNewContract();
 
         protected abstract double GetPosition();
+
+        protected abstract double ContractPrice();
     }
 }
---
+++ CryptoTrader/Strategy/HedgingStrategy/GeneralMerton.cs
@@ -0,0 +1,27 @@
+﻿using CryptoTrader.Helper.Candle;
+using CryptoTrader.HistoryAnalyzer;
+
+namespace CryptoTrader.Strategy.HedgingStrategy
+{
+    public class GeneralMerton : AHedgingStrategy
+    {
+        public GeneralMerton(IHistoryAnalyzer historyAnalyzer, CandleInterval candleInterval) : base(historyAnalyzer, candleInterval) {
+        }
+
+        protected override double MakeOrder() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void StartNewContract() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override double GetPosition() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override double ContractPrice() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
